193a4771dfa8f45e89f3184bb0a8f5e3ae6ac62c
Steven "Kreuvf" Koenig <steven.koenig.wzs@kreuvf.de>
2020-05-22 19:45:39 +0200

    * Add snippet with consequences file list

---
+++ tex/res/consequences/consequences.py
@@ -0,0 +1,18 @@
+# This file contains the list of all consequences files
+
+# I want this information to be around only once for all scripts
+
+files =	[
+		'cut.json',
+		'obtrusive-0.json',
+		'obtrusive-1.json',
+		'obtrusive-2.json',
+		'overwhelmed.json',
+		'pickpocket.json',
+		'premium.json',
+		'surprise.json',
+		'under-observation.json',
+		'wrong-person-0.json',
+		'wrong-person-1.json',
+		'wrong-person-2.json'
+	]
